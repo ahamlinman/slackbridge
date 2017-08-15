2f954ddd868b9de05e4a0b884dc40fccf40df1c5
Alex Hamlin <alex@alexhamlin.co>
2017-08-14 21:46:42 -0700

    Exit less verbosely on cobra errors
    Just use status code 1 rather than panic. The latter isn't really
    necessary.

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import "gitlab.alexhamlin.co/go/slackbridge/cmd"
+import (
+	"os"
+
+	"gitlab.alexhamlin.co/go/slackbridge/cmd"
+)
 
 func main() {
 	if err := cmd.RootCmd.Execute(); err != nil {
-		panic(err)
+		os.Exit(1)
 	}
 }
