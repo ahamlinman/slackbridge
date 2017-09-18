4c68dd55c1c681665861a75dbe82579333d058bf
Alex Hamlin <alex@alexhamlin.co>
2017-09-17 17:34:29 -0700

    Snapshot first draft of childproc logic
    This is a draft version of the childproc code that is really not that
    good. There's a lot of weird closing and error handling going on that I
    think can be simplified a good deal. In particular, I want copy
    goroutines themselves to start owning more of the pipe closure logic
    themselves. Then I can just wrap them all up in a WaitGroup and
    termination becomes a lot easier to think about. I hope.

--- childproc/childproc.go
+++ childproc/childproc.go
@@ -20,6 +20,7 @@ package childproc
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"sync"
@@ -27,7 +28,16 @@ import (
 
 // Process is the type for a child process managed by package childproc.
 type Process struct {
-	process      *os.Process
+	process *os.Process
+
+	readerCloser io.Closer
+	writerCloser io.Closer
+
+	childInRead   *os.File // reference kept to drain pipe after child exit
+	childInWrite  *os.File
+	childOutRead  *os.File
+	childOutWrite *os.File
+
 	shutdownOnce sync.Once
 	err          error
 }
@@ -35,25 +45,54 @@ type Process struct {
 // Spawn starts a child process from the given command line (name + arguments)
 // whose standard streams are connected to the provided io.ReadCloser and
 // io.WriteCloser. After the child process terminates (either on its own or
-// through explicit action from the caller), or if an error is encountered
-// while spawning the process, the provided ReadCloser and WriteCloser will be
-// closed.
+// through explicit action from the caller) the provided ReadCloser and
+// WriteCloser will be closed.
 func Spawn(cmdline []string, stdin io.ReadCloser, stdouterr io.WriteCloser) (*Process, error) {
+	p := &Process{
+		readerCloser: stdin,
+		writerCloser: stdouterr,
+	}
+
 	// Look up based on $PATH, just like package exec
-	path := exec.LookPath(cmdline[0])
+	path, err := exec.LookPath(cmdline[0])
+	if err != nil {
+		return nil, fmt.Errorf("childproc lookup failed: %v", err)
+	}
+
+	if err := p.populateIOStreams(); err != nil {
+		return nil, fmt.Errorf("chlidproc setup failed: %v", err)
+	}
 
 	attr := &os.ProcAttr{
-		Files: []*File{}, // TODO implement this for real
+		Files: []*os.File{p.childInWrite, p.childOutRead, p.childOutRead},
 	}
 
 	proc, err := os.StartProcess(path, cmdline[1:], attr)
 	if err != nil {
-		return nil, fmt.Errorf("childproc failed to Spawn: %v", err)
+		// TODO close stuff
+		return nil, fmt.Errorf("childproc start failed: %v", err)
 	}
 
-	return &Process{
-		process: proc,
+	p.process = proc
+	return p, nil
+}
+
+func (p *Process) populateIOStreams() error {
+	// TODO stop leaking fds, but make it clean
+
+	childInRead, childInWrite, err := os.Pipe()
+	if err != nil {
+		return err
 	}
+
+	childOutRead, childOutWrite, err := os.Pipe()
+	if err != nil {
+		return err
+	}
+
+	p.childInRead, p.childInWrite = childInRead, childInWrite
+	p.childOutRead, p.childOutWrite = childOutRead, childOutWrite
+	return nil
 }
 
 // Close terminates this child process if it has not yet terminated on its own.
@@ -62,6 +101,42 @@ func Spawn(cmdline []string, stdin io.ReadCloser, stdouterr io.WriteCloser) (*Pr
 func (p *Process) Close() error {
 	p.shutdown(true)
 	return p.err
+}
+
+// closeAllIO shuts down all I/O streams referenced by this Process, including
+// the Reader and Writer provided when spawning as well as all internal pipes.
+func (p *Process) closeAllIO() error {
+	var err error
+
+	// 1. Close the input Reader, so it will return EOF and copies out of it will
+	// terminate.
+	if p.readerCloser != nil {
+		err = p.readerCloser.Close()
+	}
+
+	// 2. Close our connection to the child's stdin, draining the output to
+	// prevent EPIPEs (kind of dumb?)
+	if p.childInRead != nil {
+		done := make(chan struct{})
+		go func() {
+			// TODO assign this to err?
+			io.Copy(ioutil.Discard, p.childInRead)
+			close(done)
+		}()
+
+		err = p.childInWrite.Close()
+		<-done // TODO also wait for goroutine?
+		err = p.childInRead.Close()
+		p.childInWrite, p.childInRead = nil, nil
+	}
+
+	if p.childOutWrite != nil {
+		err = p.childOutWrite.Close()
+		// TODO wait for goroutine to terminate
+		err = p.writerCloser.Close()
+	}
+
+	return err
 }
 
 // shutdown rolls up logic that should only run once when a child process
@@ -76,5 +151,17 @@ func (p *Process) shutdown(kill bool) {
 		}
 
 		// TODO implement for real
+
+		// 1. the process terminates (above)
+
+		// 2. close io.Reader, stops io.Copy
+		// 2a. drain child stdin? to be fair we will get an EPIPE if we don't
+		// (meaning we have to reimplement per-OS os/exec logic)
+		// (but yes we also need to retain a reference to the fd in this case)
+		// 3. close os.Pipe write side (child stdin)
+
+		// 4. close os.Pipe read side (child stdout/err), stops io.Copy with nil err
+		// 5. close io.Writer
+		// (behavior of pipe read close: https://play.golang.org/p/tq8QVRLKug)
 	})
 }
